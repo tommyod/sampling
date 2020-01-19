408b201590ac4d64be934cc94b7df70d2bf638a8
mojohn89 <mojohnsen89@gmail.com>
2020-01-19 17:47:21 +0100

    Docstringed tests + gitignore (#7)

--- sampling/tests/api_tests.py
+++ sampling/tests/api_tests.py
@@ -3,15 +3,24 @@ import random
 import pytest
 
 
-@pytest.mark.parametrize("weights", [None, None])
+@pytest.mark.parametrize("weights", [None, [1, 2, 3]])
 def test_input_equals_output(weights):
+    """Test that the set of values returned from the urn are the same as the input population:
+     - With a non-weighted population
+     - With a weighted population
+    """
     data = [1, 2, 3]
     urn = Urn(data, replace=False, weights=weights)
     assert set(urn) == set(data)
 
 
-@pytest.mark.parametrize("weights", [None, None])
+@pytest.mark.parametrize("weights", [None, [1, 2, 3]])
 def test_usage(weights):
+    """Test that usage functionality of the urn is equivalent:
+     - Cast to list directly
+     - Iterate with a while loop and add to list
+     - For loop the Urn
+    """
     seed = 123
     data = [1, 2, 3]
 
@@ -19,26 +28,19 @@ def test_usage(weights):
     urn = Urn(data, replace=False, weights=weights)
     samples1 = list(urn)
 
-    # random.seed(seed)
-    # urn = Urn(data, replace=False, weights=weights)
-    # samples2 = []
+    random.seed(seed)
+    urn = Urn(data, replace=False, weights=weights)
+    samples2 = []
 
-    # while urn:
-    #   samples2.append(next(urn))
+    while urn:
+        samples2.append(next(urn))
 
     random.seed(seed)
     urn = Urn(data, replace=False, weights=weights)
     samples3 = []
 
     for item in urn:
-        print(item)
         samples3.append(item)
 
     assert samples1 == samples3
-    # assert samples2 == samples3
-
-
-if __name__ == "__main__":
-    import pytest
-
-    pytest.main(args=[__file__, "--doctest-modules", "-v", "--capture=sys"])
+    assert samples2 == samples3
---
+++ sampling/tests/tree_tests.py
@@ -0,0 +1,67 @@
+from sampling.tree import CumulativeSumTree
+import random
+import math
+import pytest
+
+
+@pytest.mark.parametrize("num_weights", [10, 100, 1000])
+def test_query(num_weights):
+    """Test that:
+     - The sum of weights to the left of the returned index is smaller 
+        than the query weight
+     - The sum of weights to the left of the return index + the weight 
+        associated with the index is greater than the query weight
+    """
+
+    random.seed(42)
+    weights = [random.random() for _ in range(num_weights)]
+    tree = CumulativeSumTree(weights)
+    for _ in range(num_weights):
+        weight = random.random() * tree.get_sum()
+        i = tree.query(weight)
+        assert 0 <= i <= num_weights - 1
+        left_sum = sum(tree[j] for j in range(len(weights)) if j < i)
+        assert left_sum < weight
+        assert left_sum + tree[i] >= weight
+
+
+@pytest.mark.parametrize("num_weights", [100, 1000, 10000])
+def test_setting_weights_to_zero(num_weights):
+    """Test that:
+     - The indices returned are unique
+    """
+
+    weights = [random.random() for _ in range(num_weights)]
+    # Realign weights for greater diff
+    weights = [-math.log(w) for w in weights]
+    indices = []
+
+    tree = CumulativeSumTree(weights)
+    for _ in range(num_weights):
+        weight = random.random() * tree.get_sum()
+        i = tree.query(weight)
+        indices.append(i)
+        tree.update_weight(i, 0)
+
+    assert len(indices) == len(set(indices))
+
+
+@pytest.mark.parametrize("scale", [1e25, 1e-25])
+def test_scale_invariance_of_weights(scale):
+    """Test that:
+     - Sampling is invariant when multiplied with a large or small constant
+    """
+
+    def sample_indices(scale):
+        random.seed(42)
+        num_weights = 100
+        weights = [scale * random.random() for _ in range(num_weights)]
+
+        tree = CumulativeSumTree(weights)
+        for _ in range(num_weights):
+            weight = random.random() * tree.get_sum()
+            i = tree.query(weight)
+            tree.update_weight(i, 0)
+            yield i
+
+    assert list(sample_indices(1)) == list(sample_indices(scale))
