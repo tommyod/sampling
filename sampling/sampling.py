8716b60580a6b03b3131830121615f8257bc8490
Jens Christian Wahl <jens.c.wahl@gmail.com>
2020-01-21 21:51:39 +0100

    Sample wrapper (#9)

--- sampling/sampling.py
+++ sampling/sampling.py
@@ -101,3 +101,75 @@ class Urn(Iterator, Sized):
                 self.population[self.num_remaining],
             )
             return self.population[self.num_remaining]
+
+
+def sample(population, size, replace=False, weights=None):
+    """
+    sample takes a sample of the specified size from the elements of population 
+    using either with or without replacement, with or without weights.
+    
+    Parameters
+    ----------
+    population: list
+        The data points. 
+    
+    replace: bool
+        Sample with or without replacement?
+        
+    weights: list
+        One weight per data point. If None is
+        passed, uniform weights are used.
+        
+    Returns
+    -------
+    list
+    Returns a new list of length size containing elements from the population 
+    while leaving the original population unchanged.
+        
+        
+    Examples
+    --------
+    >>> data = [1, 3, 4, 7]
+    >>> weights = [3, 4, 2, 1]
+    >>> sample(population=data, replace=False, weights=weights)
+    >>> sample(population=data, replace=False, weights=None)
+    
+    """
+
+    urn = Urn(population=population, replace=replace, weights=weights)
+    return list(itertools.islice(urn, size))
+
+
+if __name__ == "__main__":
+    """
+    tree = CumulativeSumTree([0.2, 0.3, 0.1, 0.4, 0.8])
+    print(tree.bst)
+    print(tree.right_sums)
+    print(tree.query(0.55))
+    print(tree.query(0.61))
+    for _ in range(100):
+        weight = random.random() * tree.get_sum()
+        i = tree.query(weight)
+        left_sum = sum(tree[j] for j in range(100) if j < i)
+        assert left_sum < weight
+        assert left_sum + tree[i] > weight
+    """
+
+    size = 8
+    weights = [i * 0.1 for i in range(size)]
+    indices = []
+
+    tree = CumulativeSumTree(weights)
+    for _ in range(size):
+        weight = random.random() * tree.get_sum()
+        i = tree.query(weight)
+        # print(i)
+        # print(tree.bst)
+        # print(tree.right_sums)
+        indices.append(i)
+        tree.update_weight(i, 0)
+
+    assert len(indices) == len(set(indices))
+    # print(tree.bst)
+    # print(tree.right_sums)
+    # print(tree.query(0.55))
