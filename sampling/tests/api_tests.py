2376185726b52e012e9bcb54f34229a2e30025b9
tommyod <tommy.odland@gmail.com>
2020-02-02 15:54:55 +0100

    removed static methods in favor of normal methods in test class

--- sampling/tests/api_tests.py
+++ sampling/tests/api_tests.py
@@ -6,8 +6,7 @@ import pytest
 
 class TestUrn:
     @pytest.mark.parametrize("weights", [None, [1, 2, 3]])
-    @staticmethod
-    def test_input_equals_output(weights):
+    def test_input_equals_output(self, weights):
         """Test that the set of values returned from the urn are the same as the input population:
          - With a non-weighted population
          - With a weighted population
@@ -17,8 +16,7 @@ class TestUrn:
         assert set(urn) == set(data)
 
     @pytest.mark.parametrize("weights", [None, [1, 2, 3]])
-    @staticmethod
-    def test_urn_bool(weights):
+    def test_urn_bool(self, weights):
         """Test that bool(urn) works as expected.
         """
         data = [1, 2, 3]
@@ -32,8 +30,7 @@ class TestUrn:
         assert not bool(urn)
 
     @pytest.mark.parametrize("weights", [None, [1, 2, 3]])
-    @staticmethod
-    def test_urn_length_without_replacement(weights):
+    def test_urn_length_without_replacement(self, weights):
         """Test that urn.size() works as expected.
         """
         data = [1, 2, 3]
@@ -45,8 +42,7 @@ class TestUrn:
         assert urn.size() == 0
 
     @pytest.mark.parametrize("weights", [None, [1, 2, 3]])
-    @staticmethod
-    def test_urn_length_with_replacement(weights):
+    def test_urn_length_with_replacement(self, weights):
         """Test that urn.size() works as expected.
         """
         data = [1, 2, 3]
@@ -62,16 +58,14 @@ class TestUrn:
         assert urn.size() == float("inf")
 
     @pytest.mark.parametrize("type_func, replace", list(itertools.product((list, set, tuple, str), (True, False))))
-    @staticmethod
-    def test_input_types_unweighted(type_func, replace):
+    def test_input_types_unweighted(self, type_func, replace):
         """Test that common Python types work."""
         data = type_func("abcdef")
         urn = Urn(data, replace=replace, weights=None)
         next(urn)
 
     @pytest.mark.parametrize("type_func, replace", list(itertools.product((list, tuple), (True, False))))
-    @staticmethod
-    def test_input_types_weighted(type_func, replace):
+    def test_input_types_weighted(self, type_func, replace):
         """Test that common Python types work."""
         data = type_func("abcdef")
         weights = type_func(i + 1 for i in range(len(data)))
@@ -79,8 +73,7 @@ class TestUrn:
         next(urn)
 
     @pytest.mark.parametrize("weights", [None, [1, 2, 3]])
-    @staticmethod
-    def test_usage(weights):
+    def test_usage(self, weights):
         """Test that usage functionality of the urn is equivalent:
          - Cast to list directly
          - Iterate with a while loop and add to list
